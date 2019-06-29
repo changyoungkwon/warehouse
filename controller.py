7f2207851437c7036cffb6b601438a94418b1297
changyoung kwon <changyoung.kwon.illugrammer@gmail.com>
2019-06-29 23:04:15 +0900

    initial commit

---
+++ controller.py
@@ -0,0 +1,15 @@
+
+def upload_mrdata( filename ) : 
+    """
+
+    """
+    return
+
+def download_mrdata( filename ) : 
+    return
+
+def fetch_mrdata( header_only=False ) : 
+    return
+
+def statistics() : 
+    return
